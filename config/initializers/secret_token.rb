2c84cfc5c456b353ab4b9f8df69f54c0b0301aa9
denisdbell <denisdbell@denisdbell-ThinkPad-T430.(none)>
2014-04-12 19:30:20 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wehdehpondimenu::Application.config.secret_token = '920eb2c01b560d4bcf45612d02d990d41691e068764cd32272dc62f6823ab78ffdd4225db758a18c8ecb0dfd90d8b2ef0f99d618e023a2a714209d88bf7a6ae4'
